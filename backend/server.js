a9a90c315c73fd12eeaf0958e0d68904dd5e0ac2
Kundan Prasad <kp800532@gmail.com>
2023-07-14 01:42:08 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require("./app");
+const { path } = require("./app");
+const connectDatabase = require("./config/database");
+const cloudinary = require("cloudinary");
+
+// Configure
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({path:"backend/config/config.env"});        
+}
+
+// Connecting to database
+connectDatabase()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server listening on http://localhost:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting Down The Server Due To Unhandled Promise Rejection`);
+
+    server.close( () => {
+        process.exit(1);
+    });
+});
